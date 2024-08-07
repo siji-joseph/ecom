1b9dc151b8adc9599692ede1f3c6d19c5770d38e
Siji <sijijoseph95@gmail.com>
2024-08-08 11:02:27 +1200

    user role authentication

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'rolemanager:customer'])->name('dashboard');
+
+Route::get('/admin/dashbord', function () {
+    return view('admin');
+})->middleware(['auth', 'verified','rolemanager:admin'])->name('admin');
+
+Route::get('/vendor/dashboard', function () {
+    return view('vendor');
+})->middleware(['auth', 'verified', 'rolemanager:vendor'])->name('vendor');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
